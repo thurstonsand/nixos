6ddcf867b4801512d2f9a3108862354a8b243b4d
Thurston Sandberg <thurstonsand@hey.com>
2024-01-01 23:16:33 -0500

    add mosquitto

---
+++ containers/mosquitto.nix
@@ -0,0 +1,26 @@
+{ mosquitto-ip }:
+{
+  virtualisation.enhanced-containers = {
+    mosquitto = {
+      image = "eclipse-mosquitto:latest";
+      user = "3001:3001";
+      ip = mosquitto-ip;
+      ports = [
+        "1883:1883"
+        "8883:8883"
+        "9001:9001"
+      ];
+      environment = {
+        RUN_INSECURE_MQTT_SERVER = "0";
+        PUID = "3001";
+        PGID = "3001";
+      };
+      volumes = [
+        "/apps/mosquitto/config:/mosquitto/config"
+        "/apps/mosquitto/data:/mosquitto/data"
+        "/apps/mosquitto/log:/mosquitto/log"
+        "/etc/localtime:/etc/localtime:ro"
+      ];
+    };
+  };
+}
