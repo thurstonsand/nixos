7d8e51f0cb5c9b34d81f2c5281415c2ebba3f214
Thurston Sandberg <thurstonsand@hey.com>
2024-01-19 13:47:42 -0500

    add unifi-client-check

---
+++ nas/containers/unifi-client-check.nix
@@ -0,0 +1,16 @@
+{ unifi-client-check-ip, secrets }:
+{
+  virtualisation.enhanced-containers = {
+    unifi-client-check = {
+      image = "zsamuels28/unificlientalerts:latest";
+      ip = unifi-client-check-ip;
+      environment = {
+        "UNIFI_CONTROLLER_USER" = "localadmin";
+        "UNIFI_CONTROLLER_PASSWORD" = secrets.localadmin-password;
+        "UNIFI_CONTROLLER_URL" = "https://192.168.1.1:443";
+        "TELEGRAM_BOT_TOKEN" = secrets.telegram-bot-token;
+        "TELEGRAM_CHAT_ID" = secrets.telegram-chat-id;
+      };
+    };
+  };
+}
