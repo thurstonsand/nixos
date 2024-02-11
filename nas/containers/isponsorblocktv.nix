9d2a0446979934f06502ec9a776cf6c7431f6595
Thurston Sandberg <thurstonsand@hey.com>
2024-02-11 14:28:17 -0500

    pin isponsorblock until it is fixed

--- nas/containers/isponsorblocktv.nix
+++ nas/containers/isponsorblocktv.nix
@@ -1,7 +1,7 @@
 {
   virtualisation.enhanced-containers = {
     isponsorblocktv = {
-      image = "ghcr.io/dmunozv04/isponsorblocktv";
+      image = "ghcr.io/dmunozv04/isponsorblocktv:v2.0.4";
       volumes = [
         "/apps/isponsorblocktv/app/data:/app/data"
       ];
