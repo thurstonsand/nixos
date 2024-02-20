4c13c666a83dcb33b4a926b071054fb074d22a21
Thurston Sandberg <thurstonsand@hey.com>
2024-02-19 23:48:47 -0500

    remove unused services

--- nas/containers/default.nix
+++ nas/containers/default.nix
@@ -5,14 +5,11 @@ with pkgs;
 let
   macvlan-name = "homenet";
   vlans = import ../vlans.nix;
-  tailscaled-ip = "192.168.1.102";
   gluetun-ip = "192.168.1.195";
   cloudflared-ip = "192.168.1.202";
   homeassistant-ip = "192.168.1.205";
   zwave-js-ui-ip = "192.168.1.206";
   scrypted-ip = "192.168.1.210";
-  send-ip = "192.168.1.224";
-  send-redis-ip = "192.168.1.225";
   overseerr-ip = "192.168.1.228";
   flaresolverr-ip = "192.168.1.230";
   homarr-ip = "192.168.1.231";
@@ -23,7 +20,6 @@ let
   prowlarr-ip = "192.168.1.241";
   torrent-restarter-ip = "192.168.1.242";
 
-  ntp-server-ip = "192.168.6.225";
   # various secrets that these containers need
   secrets = with builtins; fromJSON (readFile ./secrets.json);
 in
@@ -33,21 +29,23 @@ in
     ./watchtower.nix
     ./ddclient.nix
     ./isponsorblocktv.nix
-    (import ./tailscaled.nix { inherit tailscaled-ip; auth-key = secrets.tailscale-auth-key; })
     (import ./torrent.nix { inherit gluetun-ip torrent-restarter-ip; secrets = secrets.torrent; })
     (import ./cloudflared.nix { inherit cloudflared-ip; token = secrets.cloudflare-token; })
     (import ./homeassistant.nix { inherit homeassistant-ip; })
     (import ./zwave-js-ui.nix { inherit zwave-js-ui-ip; })
     (import ./scrypted.nix { inherit scrypted-ip; })
-    (import ./ntp-server.nix { inherit ntp-server-ip; })
     (import ./arr-apps.nix { inherit flaresolverr-ip prowlarr-ip sonarr-ip radarr-ip overseerr-ip; })
-    (import ./send.nix { inherit send-redis-ip send-ip; })
     (import ./homarr.nix { inherit homarr-ip; })
     (import ./mosquitto.nix { inherit mosquitto-ip; })
     (import ./unifi-client-check.nix {
       inherit unifi-client-check-ip;
       secrets = secrets.unifi-client-check;
     })
+
+    # no longer using
+    # (import ./ntp-server.nix { inherit ntp-server-ip; })
+    # (import ./tailscaled.nix { inherit tailscaled-ip; auth-key = secrets.tailscale-auth-key; })
+    # (import ./send.nix { inherit send-redis-ip send-ip; })
   ];
   config = {
     # network drives
