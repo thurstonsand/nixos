b8335d587520f9161bfe9a19c626eb3dbf159b47
Thurston Sandberg <thurstonsand@gmail.com>
2024-04-14 11:10:52 -0400

    full path to script for systemd

--- nas/containers/isponsorblocktv.nix
+++ nas/containers/isponsorblocktv.nix
@@ -1,7 +1,7 @@
 {
   virtualisation.enhanced-containers = {
     isponsorblocktv = {
-      image = "ghcr.io/dmunozv04/isponsorblocktv:v2.0.4";
+      image = "ghcr.io/dmunozv04/isponsorblocktv";
       volumes = [
         "/apps/isponsorblocktv/app/data:/app/data"
       ];
