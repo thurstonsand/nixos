dbb9ee9ccca6332e6a0f585df79cea0c3fc9211b
Thurston Sandberg <thurstonsand@gmail.com>
2024-05-20 23:44:25 -0400

    update deps

--- nas/containers/unifi-client-check.nix
+++ nas/containers/unifi-client-check.nix
@@ -5,6 +5,9 @@
       image = "zsamuels28/unificlientalerts:latest";
       mac-address = "aa:3b:95:d2:8a:cb";
       ip = unifi-client-check-ip;
+      volumes = [
+        "/apps/unifi-client-check/usr/src/myapp/src/knownMacs.db:/usr/src/myapp/src/knownMacs.db"
+      ];
       environment = {
         "REMEMBER_NEW_DEVICES" = "true";
         "UNIFI_CONTROLLER_USER" = "localadmin";
