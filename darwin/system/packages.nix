17be4c7cbf4d31fd0b635e653779426e6df83f58
Thurston Sandberg <thurstonsand@gmail.com>
2024-12-27 02:52:34 +0800

    symlink `windsurf` to `code` (for gitlens, which hard-codes `code`)

--- darwin/system/packages.nix
+++ darwin/system/packages.nix
@@ -2,5 +2,13 @@
   environment.systemPackages = with pkgs; [
     imagemagick
     nil
+    (symlinkJoin {
+      name = "code"; # Name of the resulting package
+      paths = []; # Empty because we're not combining existing packages
+      postBuild = ''
+        mkdir -p $out/bin
+        ln -s /opt/homebrew/bin/windsurf $out/bin/code
+      '';
+    })
   ];
 }
